363058b4875776a71c6862bca1928032fa809e81
noorhonjol <s12011175@stu.najah.edu>
2023-11-22 00:05:32 +0200

    adding random color generator

--- client-side/src/Card.jsx
+++ client-side/src/Card.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/prop-types */
-
-const Card=({type,description,icon,clr})=>{
-    const dynamicClass = `bg-${clr}`;
+import randomColor from "randomcolor";
+const Card=({type,description,icon,name})=>{
     return(
-        <section className={`${dynamicClass} px-10 h-1/2`}>
+        <section style={{backgroundColor:randomColor({"luminosity":"light"})}} className={`px-10 h-1/2`}>
+            {name}
             <img className="mt-10" src={icon}/>
-            <h1 className=" text-4xl mt-12">{type}</h1>
-            <p className="mt-12 text-md">{description}</p>
+            <h1 style={{color:"gray"}} className=" text-4xl mt-12">{type}</h1>
+            <p  style={{color:"gray"}} className="mt-12 text-md">{description}</p>
         </section>
     )
-
-}
+    }
 export default Card
--- client-side/src/MainPage.jsx
+++ client-side/src/MainPage.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Card from "./Card";
 import { useLanguageContext } from './useLanguageContext';
 
 const MainPage = () => {
+    const [count,setCount]=useState(0)
     const data = useLanguageContext();
     if (!data) {
         return <p>Loading...</p>;
@@ -9,8 +11,13 @@ const MainPage = () => {
     return (
         <div className="rounded-lg h-screen w-3/6 mx-auto flex items-center">
             {data.data.map((car, index) => (
-                <Card key={index} type={car.type} description={car.description} icon={car.icon} clr={car.clr} />
+                <Card key={index} 
+                type={car.type} 
+                count={count} 
+                description={car.description}
+                icon={car.icon}/>
             ))}
+            <button onClick={()=>setCount(count+1)}></button>
         </div>
     );
 };
